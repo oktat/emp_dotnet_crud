1f0f56138562d36969a25edde2f473c9f6c893d3
Sallai András <andrewteki@gmail.com>
2024-02-04 14:00:10 +0100

    Kezdés

---
+++ DataService.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace app01.Data;
+
+public class DataService : DbContext {
+    public DataService(DbContextOptions<DataService> options)
+        :base(options) {}
+
+    public DbSet<Employee> Employees {get; set;} = null!;
+}
---
+++ Employee.cs
@@ -0,0 +1,9 @@
+namespace app01;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? City { get; set; }
+    public double Salary { get; set; }
+}
